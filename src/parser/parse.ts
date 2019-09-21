8d1f6690b3a118599c58382c067ec016f5774868
Kevin Phelps <KevinPhelps11@gmail.com>
2019-09-21 13:14:01 -0500

    feat: add initial implementation
    https://www.destroyallsoftware.com/screencasts/catalog/a-compiler-from-scratch

---
+++ src/parser/parse.ts
@@ -0,0 +1,141 @@
+import { Token, TokenType } from './../tokenizer/token';
+import { DefNode, ExpressionNode, FunctionCallNode, IntegerLiteralNode, VariableReferenceNode } from './nodes';
+
+export function parse(tokens: Token[]) {
+  tokens = [...tokens]; // preserve original array
+
+  if (tokens.length) {
+    if (peek(TokenType.DefKeyword, tokens)) {
+      const defNode = parseDef(tokens);
+
+      if (tokens.length) {
+        throw new Error(getMessageWithToken('Expected end of program', tokens[0]));
+      }
+
+      return defNode;
+    } else {
+      throw new Error(getMessageWithToken("Expected 'def' keyword at start of program", tokens[0]));
+    }
+  } else {
+    return null as DefNode;
+  }
+}
+
+function parseDef(tokens: Token[]) {
+  consume(TokenType.DefKeyword, tokens);
+  const name = parseIdentifier(tokens);
+  const parameterNames = parseParameterNames(tokens);
+  const body = parseExpression(tokens);
+  consume(TokenType.EndKeyword, tokens);
+
+  return new DefNode(name, parameterNames, body);
+}
+
+function parseIdentifier(tokens: Token[]) {
+  return consume(TokenType.Identifier, tokens).value;
+}
+
+function parseParameterNames(tokens: Token[]) {
+  const parameterNames: string[] = [];
+
+  consume(TokenType.OpenParen, tokens);
+
+  if (peek(TokenType.Identifier, tokens)) {
+    pushNextParameterName();
+
+    while (peek(TokenType.Comma, tokens)) {
+      consume(TokenType.Comma, tokens);
+      pushNextParameterName();
+    }
+  }
+
+  consume(TokenType.CloseParen, tokens);
+
+  return parameterNames;
+
+  function pushNextParameterName() {
+    if (!peek(TokenType.Identifier, tokens)) {
+      throw new Error(getMessageWithToken('Expected next identifier in parameter list', tokens[0]));
+    }
+
+    parameterNames.push(parseIdentifier(tokens));
+  }
+}
+
+function parseExpression(tokens: Token[]) {
+  if (peek(TokenType.Integer, tokens)) {
+    return parseIntegerLiteral(tokens);
+  } else if (peek(TokenType.Identifier, tokens) && peek(TokenType.OpenParen, tokens, 1)) {
+    return parseFunctionCall(tokens);
+  } else if (peek(TokenType.Identifier, tokens)) {
+    return parseVariableRefernce(tokens);
+  } else {
+    return null;
+  }
+}
+
+function parseIntegerLiteral(tokens: Token[]) {
+  const integerToken = consume(TokenType.Integer, tokens);
+
+  return new IntegerLiteralNode(parseInt(integerToken.value, 10));
+}
+
+function parseVariableRefernce(tokens: Token[]) {
+  const identifierToken = consume(TokenType.Identifier, tokens);
+
+  return new VariableReferenceNode(identifierToken.value);
+}
+
+function parseFunctionCall(tokens: Token[]) {
+  return new FunctionCallNode(parseIdentifier(tokens), parseParameters(tokens));
+}
+
+function parseParameters(tokens: Token[]) {
+  const parameters: ExpressionNode[] = [];
+
+  consume(TokenType.OpenParen, tokens);
+
+  if (!peek(TokenType.CloseParen, tokens)) {
+    pushNextExpression();
+
+    while (peek(TokenType.Comma, tokens)) {
+      consume(TokenType.Comma, tokens);
+      pushNextExpression();
+    }
+  }
+
+  consume(TokenType.CloseParen, tokens);
+
+  return parameters;
+
+  function pushNextExpression() {
+    const expression = parseExpression(tokens);
+
+    if (!expression) {
+      throw new Error(getMessageWithToken('Expected start of next expression in parameter list', tokens[0]));
+    }
+
+    parameters.push(expression);
+  }
+}
+
+function consume(expectedTokenType: TokenType, tokens: Token[]) {
+  const token = tokens.shift();
+
+  if (!token) {
+    throw new Error(`Expected a '${expectedTokenType}' token, but got nothing.`);
+  } else if (token.type !== expectedTokenType) {
+    throw new Error(getMessageWithToken(`Expected a '${expectedTokenType}' token`, token));
+  }
+
+  return token;
+}
+
+function peek(expectedTokenType: TokenType, tokens: Token[], index = 0) {
+  const token = tokens[index];
+  return token && token.type === expectedTokenType;
+}
+
+function getMessageWithToken(expectedMessage: string, token: Token) {
+  return `${expectedMessage}, but got a '${token.type}' token with value '${token.value}'.`;
+}
--- src/toy-compiler.spec.ts
+++ src/toy-compiler.spec.ts
@@ -1,7 +1,63 @@
 import { compile } from './toy-compiler';
 
 describe('compile', () => {
-  it('should work', () => {
+  it('should compile an empty program', () => {
     expect(compile('')).toBe('');
+  });
+
+  it('should compile an empty function', () => {
+    const code = 'def f() end';
+    const js = 'function f() { }';
+
+    expect(compile(code)).toBe(js);
+  });
+
+  it('should compile a function that takes one parameter', () => {
+    const code = 'def f(a) end';
+    const js = 'function f(a) { }';
+
+    expect(compile(code)).toBe(js);
+  });
+
+  it('should compile a function that takes several parameters', () => {
+    const code = 'def f(a, b, c) end';
+    const js = 'function f(a, b, c) { }';
+
+    expect(compile(code)).toBe(js);
+  });
+
+  it('should compile a function that calls another function', () => {
+    const code = 'def f() g() end';
+    const js = 'function f() { g(); }';
+
+    expect(compile(code)).toBe(js);
+  });
+
+  it('should compile a function that calls another function with one integer literal parameter', () => {
+    const code = 'def f() g(1) end';
+    const js = 'function f() { g(1); }';
+
+    expect(compile(code)).toBe(js);
+  });
+
+  it('should compile a function that calls another function with one variable reference parameter', () => {
+    const code = 'def f() g(x) end';
+    const js = 'function f() { g(x); }';
+
+    expect(compile(code)).toBe(js);
+  });
+
+  it('should compile a function that calls another function with a nested function call', () => {
+    const code = 'def f() g(h()) end';
+    const js = 'function f() { g(h()); }';
+
+    expect(compile(code)).toBe(js);
+  });
+
+  it('should compile a function that calls another function with several different kinds of parameters', () => {
+    const code = 'def f() g(x, 1, y(z)) end';
+    const js = 'function f() { g(x, 1, y(z)); }';
+
+    expect(compile(code)).toBe(js);
   });
 });
